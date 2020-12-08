99827faa0c5dd007b7a2c87689cf755a74f0d4ee
victoriaehrbar <victoriaehrbar@gmail.com>
2020-12-08 03:09:27 +0000

    Automatically backed up by Learn

---
+++ lib/author.rb
@@ -0,0 +1,31 @@
+class Author
+  attr_accessor :name
+
+  @@all = []
+
+  def initialize(name)
+    @name = name
+    @@all << self
+  end
+
+  def self.all
+    @@all
+  end
+
+  def add_post(post)
+    song.author = self
+  end
+
+  def add_post_by_title(post, title)
+    song = Song.new(name)
+    song.artist = self
+  end
+
+  def posts
+    Song.all.select {|song| song.artist == self}
+  end
+
+  def self.post_count
+    Song.all.count
+  end
+end
